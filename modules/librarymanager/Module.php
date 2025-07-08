94968beb038cdd230587a6b15810110d510e0e3e
brijesh rupavatiya <brijesh.rupavatiya@kalkani.in>
2025-07-08 11:51:23 +0530

    Book add and display from book manager

--- modules/librarymanager/Module.php
+++ modules/librarymanager/Module.php
@@ -1,14 +1,71 @@
 <?php
 
+// namespace modules\librarymanager;
+
+// use Craft;
+// use craft\web\View;
+// use yii\base\Event;
+// use craft\web\UrlManager;
+// use craft\web\Application;
+// use craft\web\twig\variables\Cp;
+// use yii\base\Module as BaseModule;
+// use craft\events\RegisterUrlRulesEvent;
+// use craft\events\RegisterCpNavItemsEvent;
+
+// class Module extends BaseModule
+// {
+//     public function init()
+//     {
+//         parent::init();
+
+//         // Register CP Template Root
+//         Event::on(
+//             View::class,
+//             View::EVENT_REGISTER_CP_TEMPLATE_ROOTS,
+//             function ($event) {
+//                 $event->roots['librarymanager'] = __DIR__ . '/templates';
+//             }
+//         );
+
+//         // Register CP URL rules
+//         Event::on(
+//             UrlManager::class,
+//             UrlManager::EVENT_REGISTER_CP_URL_RULES,
+//             function (RegisterUrlRulesEvent $event) {
+//                 $event->rules['librarymanager'] = 'librarymanager/default/index';
+//                 $event->rules['librarymanager/books'] = 'librarymanager/default/books';
+//                 $event->rules['librarymanager/customers'] = 'librarymanager/default/customers';
+//                 $event->rules['librarymanager/borrow'] = 'librarymanager/default/borrow';
+//                 $event->rules['librarymanager/return'] = 'librarymanager/default/return';
+//             }
+//         );
+
+//         // Add item to sidebar
+//         Event::on(
+//             Cp::class,
+//             Cp::EVENT_REGISTER_CP_NAV_ITEMS,
+//             function (RegisterCpNavItemsEvent $event) {
+//                 $event->navItems[] = [
+//                     'label' => 'Book Manager',
+//                     'url' => 'librarymanager',
+//                     'icon' => '@app/icons/book.svg', // Or null if you don’t have an icon
+//                 ];
+//             }
+//         );
+//     }
+// }
+
+
+
+
 namespace modules\librarymanager;
 
 use Craft;
 use craft\web\View;
-use yii\base\Event;
 use craft\web\UrlManager;
-use craft\web\Application;
 use craft\web\twig\variables\Cp;
 use yii\base\Module as BaseModule;
+use yii\base\Event;
 use craft\events\RegisterUrlRulesEvent;
 use craft\events\RegisterCpNavItemsEvent;
 
@@ -16,9 +73,12 @@ class Module extends BaseModule
 {
     public function init()
     {
+        // ✅ Define @modules path alias (very important)
+        Craft::setAlias('@modules', dirname(__DIR__));
+
         parent::init();
 
-        // Register CP Template Root
+        // ✅ Register CP Template Root
         Event::on(
             View::class,
             View::EVENT_REGISTER_CP_TEMPLATE_ROOTS,
@@ -27,7 +87,7 @@ class Module extends BaseModule
             }
         );
 
-        // Register CP URL rules
+        // ✅ Register CP URL rules
         Event::on(
             UrlManager::class,
             UrlManager::EVENT_REGISTER_CP_URL_RULES,
@@ -40,7 +100,7 @@ class Module extends BaseModule
             }
         );
 
-        // Add item to sidebar
+        // ✅ Add sidebar nav item
         Event::on(
             Cp::class,
             Cp::EVENT_REGISTER_CP_NAV_ITEMS,
@@ -48,7 +108,7 @@ class Module extends BaseModule
                 $event->navItems[] = [
                     'label' => 'Book Manager',
                     'url' => 'librarymanager',
-                    'icon' => '@app/icons/book.svg', // Or null if you don’t have an icon
+                    'icon' => '@app/icons/book.svg', // Or use a valid path if you want a custom icon
                 ];
             }
         );
